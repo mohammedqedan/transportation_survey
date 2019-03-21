12b599ac828038b4ec3c093c09d7060e3cc0acb0
Student <example@example.com>
2019-03-20 19:10:10 -0700

    the coolness

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
+    var userResponses = [];
+  $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+      userResponses.push(workTransportationMode);
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
     });
     $('#transportation_survey').hide();
   });
